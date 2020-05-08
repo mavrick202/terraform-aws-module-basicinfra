006376bb7af21c51cee8de383fcfae60e36ffc13
Mavrick-DevOps <mavrick202@gmail.com>
2020-05-08 02:24:45 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "key_name" {}
 variable "public_subnet1_cidr" {}
 variable "public_subnet2_cidr" {}
 variable "public_subnet3_cidr" {}
-variable "public_subnet4cidr" {}
+variable "public_subnet4_cidr" {}
 variable "public_subnet1_name" {}
 variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
