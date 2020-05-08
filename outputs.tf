4ef86d81fdf2b785e1a129a593990930a29eade1
Mavrick-DevOps <mavrick202@gmail.com>
2020-05-08 13:07:02 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "subnet1" {
+  value = "${aws_subnet.subnet1-public.id}"
+}
+
+output "subnet2" {
+  value = "${aws_subnet.subnet2-public.id}"
+}
+
+output "subnet3" {
+  value = "${aws_subnet.subnet3-public.id}"
+}
